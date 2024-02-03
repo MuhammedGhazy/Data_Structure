424ae0a11dfe55cd5714460445ed2cc87c5f6233
Your Name <you@example.com>
2024-02-03 23:16:10 +0200

    QUEUE_DS

--- 2-Data_structure_BasedOnArray/QueueArray.c
+++ 2-Data_structure_BasedOnArray/QueueArray.c
@@ -1,2 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 #include "QueueArray.h"
 
+Queue_t *CreatQueue (uint32 max_size)
+{
+
+}
+queue_status_t EnqueueElement (Queue_t *queue_obj, void *Item_ptr)
+{
+	queue_status_t status = QUEUE_NOK;
+
+	if((NULL == queue_obj) || (NULL == Item_ptr))
+	{
+		status = QUEUE_NOK;
+		printf("Erorr!! This Is Invalid Adress, Please Enter Valid Adress.\n");
+	}
+	else
+	{
+		status = QUEUE_OK;
+		printf("Okay, Valid Adress.\n");
+	}
+	return (status);
+
+}
+void *DqueueElement (Queue_t *queue_obj, queue_status_t *ret_status)
+{
+	 queue_status_t status = QUEUE_NOK;
+
+        if((NULL == queue_obj) || (NULL == ret_status))
+        {
+                status = QUEUE_NOK;
+                printf("Erorr!! This Is Invalid Adress, Please Enter Valid Adress.\n");
+        }
+        else
+        {
+                status = QUEUE_OK;
+                printf("Okay, Valid Adress.\n");
+        }
+
+
+
+}
+void *QueueFront (Queue_t *queue_obj, queue_status_t *ret_status)
+{
+	queue_status_t status = QUEUE_NOK;
+
+        if((NULL == queue_obj) || (NULL == ret_status))
+        {
+                status = QUEUE_NOK;
+                printf("Erorr!! This Is Invalid Adress, Please Enter Valid Adress.\n");
+        }
+        else
+        {
+                status = QUEUE_OK;
+                printf("Okay, Valid Adress.\n");
+        }
+        
+
+}
+void *QueueRear (Queue_t *queue_obj, queue_status_t *ret_status)
+{
+	 queue_status_t status = QUEUE_NOK;
+
+        if((NULL == queue_obj) || (NULL == ret_status))
+        {
+                status = QUEUE_NOK;
+                printf("Erorr!! This Is Invalid Adress, Please Enter Valid Adress.\n");
+        }
+        else
+        {
+                status = QUEUE_OK;
+                printf("Okay, Valid Adress.\n");
+        }
+        
+
+}
+queue_status_t GetQueueCount (Queue_t *queue_obj, uint32 *QueueCount)
+{
+	 queue_status_t status = QUEUE_NOK;
+
+        if((NULL == queue_obj) || (NULL == QueueCount))
+        {
+                status = QUEUE_NOK;
+                printf("Erorr!! This Is Invalid Adress, Please Enter Valid Adress.\n");
+        }
+        else
+        {
+                status = QUEUE_OK;
+                printf("Okay, Valid Adress.\n");
+        }
+        return (status);
+
+}
+queue_status_t DestroyQueue (Queue_t *queue_obj)
+{
+	 queue_status_t status = QUEUE_NOK;
+
+        if(NULL == queue_obj)
+        {
+                status = QUEUE_NOK;
+                printf("Erorr!! This Is Invalid Adress, Please Enter Valid Adress.\n");
+        }
+        else
+        {
+                status = QUEUE_OK;
+                printf("Okay, Valid Adress.\n");
+        }
+        return (status);
+
+}
--- 2-Data_structure_BasedOnArray/QueueArray.h
+++ 2-Data_structure_BasedOnArray/QueueArray.h
@@ -18,4 +18,11 @@ typedef enum
 	QUEUE_OK
 }queue_status_t;
 
+Queue_t *CreatQueue (uint32 max_size);
+queue_status_t EnqueueElement (Queue_t *queue_obj, void *Item_ptr);
+void *DqueueElement (Queue_t *queue_obj, queue_status_t *ret_status);
+void *QueueFront (Queue_t *queue_obj, queue_status_t *ret_status);
+void *QueueRear (Queue_t *queue_obj, queue_status_t *ret_status);
+queue_status_t GetQueueCount (Queue_t *queue_obj, uint32 *QueueCount);
+queue_status_t DestroyQueue (Queue_t *queue_obj);
 #endif // QUEUE_ARRAY_H
